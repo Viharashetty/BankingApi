ecfd4dc67856e0d6f59b320d7c991eda4f125ac3
Admin <Admin@DESKTOP-H1GQ6RV>
2021-11-10 12:17:55 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using BankingApi.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankingApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+
+
+    {
+
+        private BankContext _dbContext;
+        public HomeController(BankContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpGet]
+        public float Get(int AdminID)
+        {
+
+            var user = _dbContext.Banks.Find(AdminID);
+            var amount = user.Amount;
+
+            return amount;
+
+        }
+    }
+}
+
+
+
+
+
+
+
